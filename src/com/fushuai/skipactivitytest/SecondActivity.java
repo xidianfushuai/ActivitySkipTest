40367dadad00cbaa73014a5e62a70d8659d78f76
xidianfushuai <18792514506@163.com>
2016-03-03 14:19:00 +0800

    JOB DONE

--- src/com/fushuai/skipactivitytest/SecondActivity.java
+++ src/com/fushuai/skipactivitytest/SecondActivity.java
@@ -4,16 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class SecondActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_second);
 	}
 	public void click(View view) {
-		Intent intent = new Intent(this, SecondActivity.class);
+		Intent intent = new Intent(this, MainActivity.class);
 		startActivity(intent);
 		finish();
 	}
